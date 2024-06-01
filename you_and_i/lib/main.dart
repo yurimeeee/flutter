5d38c111658b122db30ba99052f0ba5299372878
yurimeeee <kplhn6@gmail.com>
2024-06-01 16:18:45 +0900

    you and i

--- you_and_i/lib/main.dart
+++ you_and_i/lib/main.dart
@@ -7,8 +7,32 @@ void main() {
 
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
 }
-
--- you_and_i/lib/screen/home_screen.dart
+++ you_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,8 +12,143 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      backgroundColor: Colors.pink[100],
+      // backgroundColor: Colors.black,
+
+      body: SafeArea(
+        // IOS 상단 노치를 벗어나서 렌더링
+        // 시스템 제공 UI 벗어나서 렌더링, 기본적으로 모든 값은 true
+        bottom: false,
+        child: Column(
+          children: [
+            _Top(),
+            _Bottom(),
+          ],
+        ),
+      ),
     );
   }
 }
 
+class _Top extends StatefulWidget {
+  const _Top({super.key});
+
+  @override
+  State<_Top> createState() => _TopState();
+}
+
+class _TopState extends State<_Top> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Container(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width, // 현재 디바이스의 너비
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60.0,
+              ),
+              Text(
+                'You & I',
+                style: textTheme.displayLarge,
+              ),
+              Text(
+                '널 만난지',
+                style: textTheme.bodyLarge,
+              ),
+              Text(
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: textTheme.bodyMedium,
+              ),
+              // GestureDetector(
+              //   onTap: (){
+              //     showCupertinoDialog(
+              //       context: context,
+              //       barrierDismissible: true, // 외부 공간 클릭하면 Dialog 닫힘
+              //       builder: (BuildContext context) {
+              //         return Align(
+              //           alignment: Alignment.center, // 위치를 정의하지 않으면 사이즈 설정 불가
+              //           child: Container(
+              //             color: Colors.white,
+              //             height: 300.0,
+              //             child: CupertinoDatePicker(
+              //               mode: CupertinoDatePickerMode.date,
+              //               dateOrder: DatePickerDateOrder.ymd,
+              //               onDateTimeChanged: (DateTime date) {
+              //                 //선택한 날짜를 콜백으로 받음
+              //                 setState(() {
+              //                   selectedDate = date;
+              //                 });
+              //               },
+              //             ),
+              //           ),
+              //         );
+              //       },
+              //     );
+              //   },
+              //   child: SizedBox(
+              //     height: 100.0,
+              //     child: Image.asset('asset/img/heart.png', fit: BoxFit.cover,),
+              //   ),
+              // ),
+              IconButton(
+                onPressed: () {
+                  showCupertinoDialog(
+                    context: context,
+                    barrierDismissible: true, // 외부 공간 클릭하면 Dialog 닫힘
+                    builder: (BuildContext context) {
+                      return Align(
+                        alignment: Alignment.center, // 위치를 정의하지 않으면 사이즈 설정 불가
+                        child: Container(
+                          color: Colors.white,
+                          height: 300.0,
+                          child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.date,
+                            dateOrder: DatePickerDateOrder.ymd,
+                            onDateTimeChanged: (DateTime date) {
+                              //선택한 날짜를 콜백으로 받음
+                              setState(() {
+                                selectedDate = date;
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                icon: Icon(Icons.favorite),
+                iconSize: 60.0,
+                color: Colors.white,
+              ),
+              Text(
+                'D + ${now.difference(selectedDate).inDays + 1} ',
+                style: textTheme.displayMedium,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset('asset/img/you_and_i.png'),
+      ),
+    );
+  }
+}
